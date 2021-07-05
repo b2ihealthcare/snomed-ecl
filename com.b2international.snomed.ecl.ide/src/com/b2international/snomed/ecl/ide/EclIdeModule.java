b4ff854a1742f3d9124bc90503dda131588ea24d
András Péteri <apeteri@b2international.com>
2021-07-05 15:08:45 +0200

    SO-3984: Add highlighting dependencies to EclIdeModule

--- com.b2international.snomed.ecl.ide/src/com/b2international/snomed/ecl/ide/EclIdeModule.java
+++ com.b2international.snomed.ecl.ide/src/com/b2international/snomed/ecl/ide/EclIdeModule.java
@@ -15,13 +15,18 @@
  */
 package com.b2international.snomed.ecl.ide;
 
+import org.eclipse.xtext.ide.LexerIdeBindings;
 import org.eclipse.xtext.ide.editor.contentassist.IdeContentProposalProvider;
 import org.eclipse.xtext.ide.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
 import org.eclipse.xtext.ide.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.eclipse.xtext.parser.antlr.AntlrTokenDefProvider;
+import org.eclipse.xtext.parser.antlr.ITokenDefProvider;
 
 import com.b2international.snomed.ecl.ide.contentassist.EclIdeProposalProvider;
 import com.b2international.snomed.ecl.ide.highlighting.EclAntlrTokenToAttributeIdMapper;
 import com.b2international.snomed.ecl.ide.highlighting.EclSemanticHighlightingCalculator;
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
 
 /**
  * Use this class to register IDE components.
@@ -38,5 +43,18 @@ public class EclIdeModule extends AbstractEclIdeModule {
 	
 	public Class<? extends ISemanticHighlightingCalculator> bindISemanticHighlightingCalculator() {
 		return EclSemanticHighlightingCalculator.class;
-	}	
+	}
+	
+	// These are re-definitions of the bindings in AbstractEclUiModule, needed for IDE-based highlighting
+	public void configureHighlightingLexer(Binder binder) {
+		binder.bind(org.eclipse.xtext.parser.antlr.Lexer.class)
+			.annotatedWith(Names.named(LexerIdeBindings.HIGHLIGHTING))
+			.to(com.b2international.snomed.ecl.parser.antlr.internal.InternalEclLexer.class);
+	}
+	
+	public void configureHighlightingTokenDefProvider(Binder binder) {
+		binder.bind(ITokenDefProvider.class)
+			.annotatedWith(Names.named(LexerIdeBindings.HIGHLIGHTING))
+			.to(AntlrTokenDefProvider.class);
+	}
 }
