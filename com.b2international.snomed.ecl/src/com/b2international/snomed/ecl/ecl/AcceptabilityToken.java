78992aba0549b08faec5bebb50d710e591637ee7
András Péteri <apeteri@b2international.com>
2021-10-07 17:45:01 +0200

    feat(ecl): Add AcceptabilityToken enum for parsing and conversion...
    ...of acceptability tokens to concept identifiers

---
+++ com.b2international.snomed.ecl/src/com/b2international/snomed/ecl/ecl/AcceptabilityToken.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snomed.ecl.ecl;
+
+import java.util.Arrays;
+
+import com.google.common.base.Strings;
+
+/**
+ * Enumerates allowed acceptability keywords used in filters.
+ * 
+ * @since 1.6
+ */
+public enum AcceptabilityToken {
+	// IDs copied from SnomedConstants
+	ACCEPTABLE("accept", "900000000000549004"),
+	PREFERRED("prefer", "900000000000548007");
+
+	private final String token;
+	private final String conceptId;
+
+	private AcceptabilityToken(final String token, final String conceptId) {
+		this.token = token;
+		this.conceptId = conceptId;
+	}
+
+	public static AcceptabilityToken fromString(final String value) {
+		if (Strings.isNullOrEmpty(value)) {
+			return null;
+		}
+
+		return Arrays.stream(values())
+			.filter(d -> d.token.equalsIgnoreCase(value))
+			.findFirst()
+			.orElse(null);
+	}
+
+	public String getConceptId() {
+		return conceptId;
+	}
+	
+	@Override
+	public String toString() {
+		return token;
+	}
+}
